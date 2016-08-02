d9aadaf25954a6c5ca390602170124951cb14de0
friend8 <map@wafriv.de>
2016-08-02 19:48:04 +0200

    add fighting skills

---
+++ db_migrations/files/2016-08-02_190425_fightingSkills.php
@@ -0,0 +1,91 @@
+<?php
+
+$DB_MIGRATION = [
+    'description' => function () {
+		return 'Fighting skills';
+	},
+
+	'up' => function ($migration_metadata) {
+
+		$results = [];
+
+		$results[] = query_raw('
+            CREATE TABLE `fightingSkills` (
+                `fightingSkillId` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+                `code` VARCHAR(255) NOT NULL COLLATE "utf8_bin",
+                `conductiveProperty` ENUM("ff","ge","kk","ge/kk") NOT NULL COLLATE "utf8_bin",
+                `raiseGroup` ENUM("a","b","c","d","e") NOT NULL COLLATE "utf8_bin",
+                PRIMARY KEY (`fightingSkillId`)
+            )
+			COLLATE="utf8_bin"
+			ENGINE=InnoDB
+		');
+
+		$results[] = query_raw('
+			INSERT INTO `fightingSkills` (`code`, `conductiveProperty`, `raiseGroup`) VALUES ("crossbow", "ff", "b")
+		');
+
+		$results[] = query_raw('
+			INSERT INTO `fightingSkills` (`code`, `conductiveProperty`, `raiseGroup`) VALUES ("bow", "ff", "c")
+		');
+
+		$results[] = query_raw('
+			INSERT INTO `fightingSkills` (`code`, `conductiveProperty`, `raiseGroup`) VALUES ("dagger", "ge", "b")
+		');
+
+		$results[] = query_raw('
+			INSERT INTO `fightingSkills` (`code`, `conductiveProperty`, `raiseGroup`) VALUES ("fencingWeapons", "ge", "c")
+		');
+
+		$results[] = query_raw('
+			INSERT INTO `fightingSkills` (`code`, `conductiveProperty`, `raiseGroup`) VALUES ("chopWeapons", "kk", "c")
+		');
+
+		$results[] = query_raw('
+			INSERT INTO `fightingSkills` (`code`, `conductiveProperty`, `raiseGroup`) VALUES ("chainWeapons", "kk", "c")
+		');
+
+		$results[] = query_raw('
+			INSERT INTO `fightingSkills` (`code`, `conductiveProperty`, `raiseGroup`) VALUES ("brawl", "ge/kk", "b")
+		');
+
+		$results[] = query_raw('
+			INSERT INTO `fightingSkills` (`code`, `conductiveProperty`, `raiseGroup`) VALUES ("shield", "kk", "c")
+		');
+
+		$results[] = query_raw('
+			INSERT INTO `fightingSkills` (`code`, `conductiveProperty`, `raiseGroup`) VALUES ("sword", "ge/kk", "c")
+		');
+
+		$results[] = query_raw('
+			INSERT INTO `fightingSkills` (`code`, `conductiveProperty`, `raiseGroup`) VALUES ("spearWeapons", "ge/kk", "c")
+		');
+
+		$results[] = query_raw('
+			INSERT INTO `fightingSkills` (`code`, `conductiveProperty`, `raiseGroup`) VALUES ("throwingWeapons", "ff", "b")
+		');
+
+		$results[] = query_raw('
+			INSERT INTO `fightingSkills` (`code`, `conductiveProperty`, `raiseGroup`) VALUES ("twohandedChopWeapons", "kk", "c")
+		');
+
+		$results[] = query_raw('
+			INSERT INTO `fightingSkills` (`code`, `conductiveProperty`, `raiseGroup`) VALUES ("twohandedSword", "kk", "c")
+		');
+
+		return !in_array(false, $results);
+
+	},
+
+	'down' => function ($migration_metadata) {
+
+		$results = [];
+
+		$results[] = query_raw('
+            DROP TABLE fightingSkills
+		');
+
+		return !in_array(false, $results);
+
+	}
+];
